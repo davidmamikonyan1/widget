4f19a7a27d86b34784912d85dd1f28f5b229b90c
david <david.mushegh@gmail.com>
2024-12-16 18:44:14 +0400

    Added new style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+  // const increment = () => {
+  //   setCount(count + 1);
+  // };
 
-  const decrement = () => {
-    setCount(count - 1);
-  };
+  // const decrement = () => {
+  //   setCount(count - 1);
+  // };
 
   return (
     <div>
-      <h3>Counter Widget</h3>
+      {/* <h3>Counter Widget</h3>
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={decrement}>Decrement</button> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("counter-widget-container") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
