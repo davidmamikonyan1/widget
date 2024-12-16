02b1a3603644802fe386eb85b1fdd2d8044179e8
david <david.mushegh@gmail.com>
2024-12-16 18:46:06 +0400

    Added new style

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("counter-widget-container") as HTMLElement
 );
 root.render(
   <React.StrictMode>
