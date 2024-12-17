557ba69148d589f5e0905a22609ea7f36547182b
david <david.mushegh@gmail.com>
2024-12-17 16:03:55 +0400

    aaa

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,4 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+
 export const Hello = () => {
   return <div>Hello World</div>;
 };
+
+const el = document.querySelector("#container-widget-counter");
+
+if (el) {
+  const root = ReactDOM.createRoot(el);
+  root.render(<Hello />);
+}
