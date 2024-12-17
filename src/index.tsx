ea409b790c61098359d99a8266aab9eaa884c09d
david <david.mushegh@gmail.com>
2024-12-17 14:10:29 +0400

    Added new file

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Counter } from "./counter.tsx";
-import HelloWorld from "./helloWorld.tsx";
 
 const widgets = document.querySelectorAll(".container-widget");
 
 widgets.forEach((widget) => {
   const root = ReactDOM.createRoot(widget);
-  root.render(
-    <>
-      <App />
-      <Counter />
-      <HelloWorld />
-    </>
-  );
+  root.render(<App />);
 });
